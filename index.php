1592860478e312f38e80ddef2429836f665d5739
Adam Chabrzyk <ul0240518@edu.uni.lodz.pl>
2020-11-20 23:42:55 +0100

    Add index.php, Delete index.html, Refactor app.js

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/game.css">
+    <link rel="stylesheet" href="./public/css/font-awesome/all.min.css">
+    <link rel="shortcut icon" href="./favicon.ico">
+    <title>Ultimate Tic-Tac-Toe</title>
+</head>
+<body>
+    <div id="container">
+        <div id="game-bar">
+            <span class="player-1-info">Player 1 (X)</span>
+            <span class="player-2-info">Player 2 (O)</span>      
+        </div>
+        <div id="board">
+            <div class="box" data-box="0" data-index="0"></div>
+            <div class="box" data-box="0" data-index="1"></div>
+            <div class="box" data-box="0" data-index="2"></div>
+            <div class="box" data-box="0" data-index="3"></div>
+            <div class="box" data-box="0" data-index="4"></div>
+            <div class="box" data-box="0" data-index="5"></div>
+            <div class="box" data-box="0" data-index="6"></div>
+            <div class="box" data-box="0" data-index="7"></div>
+            <div class="box" data-box="0" data-index="8"></div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="./public/js/app.js"></script>
+</body>
+</html>
--- public/js/app.js
+++ public/js/app.js
@@ -1,105 +1,88 @@
-//Przygotowywujemy sobie zmienną na planszę
-var tablica;
-var plansza;
-var ruch_numer;
-//Tworzymy zdarzenie które uruchomi sie jak strona zostanie załadowana
-window.addEventListener("load", function(){
-    //Po kliknięciu przycisku zaczynamy grę na nowo
-    document.getElementById("przycisk").addEventListener("click", function(){
-        document.getElementById("komunikat").setAttribute("hidden", "");
-        nowa_gra("x");
-    });
-    //Przypisujemy sobie nasza zmienną do planszy żabyśmy nie musieli ciągle jej wyszukiwać
-    plansza = document.getElementById("plansza");
-    //dodajemy do plansz zdarzenie wywoływane przez kliknięcie myszki
-    plansza.addEventListener("click", function(zdarzenie){
-        //Element zdarzenie.target zawiera wskaźnik na polę na którym nastąpiło kliknięcie
-        //Sprawdzamy czy user napewno klinką w pole a nie np. trafił na siatę, czy gdzieś obok.
-        if(zdarzenie.target.nodeName != "TH" || zdarzenie.target.hasAttribute("pole"))
-            return false;
-        zdarzenie.target.setAttribute("pole", plansza.getAttribute("ruch"));
-        var ruch = 1;
-        if(plansza.getAttribute("ruch") == "x")
-        {
-            ruch = -1;
-            plansza.setAttribute("ruch", "o");
-        }
-        else
-            plansza.setAttribute("ruch", "x");
-        //Dodaje kolejny ruch
-        ruch_numer++;
-        //Oznaczam pole w tablicy
-        tablica[parseInt(zdarzenie.target.getAttribute("x"))][parseInt(zdarzenie.target.getAttribute("y"))] = ruch;
-        switch(czy_koniec())
-        {
-            //najpopularniejsza opcja wiec na poczatek sprawdzamy i jak jest to przerywamy sprawdzanie
-            case false: break;
-            //Już wiemy że koniec więc blokujemy interfejs odbierając ruch
-            case -1:
-                wyswietl_komunikat("Wygrał krzyzyk");
-                break;
-            case 1:
-                wyswietl_komunikat("Wygrało kólko");
-                break;
-            case -2:
-                wyswietl_komunikat("Remis");
-                break;
-        }
-    });
-    nowa_gra("x");
+// style font awesome dla poszczególnych graczy
+const PLAYER_1 = 'fa-times';
+const PLAYER_2 = 'fa-circle';
+
+// możliwe kombinacje
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// tablice ruchów gracza
+let moves_player_1 = [];
+let moves_player_2 = [];
+
+let turn = 1;
+
+// uchwyt do wszystkich elementów .box
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(box => {
+    box.addEventListener('click', SimpleClick, { once: true })
 });
-//Funkcja do wyświetlania komunikatu na koniec gry
-function wyswietl_komunikat(tresc)
-{
-    document.getElementById("tresc").innerHTML = tresc;
-    plansza.removeAttribute("ruch");
-    document.getElementById("komunikat").removeAttribute("hidden");
-};
-//Podajemy kto zaczyna x - krzyzyk, o - kolko
-function nowa_gra(zaczyna)
-{
-    //Resetuje liczbę ruchów
-    ruch_numer = 0;
-    //Tworzymy tabelę jednowymiarową
-    tablica = new Array(3);
-    //Dodajemy drugi wymiar tworząc w każdym wierszi tabeli kolejną tabelę
-    for(var i = 0; i < tablica.length;i++)
-    {
-        tablica[i] = new Array(tablica.length);
-        for(var x = 0; x < tablica.length; x++)
-        tablica[i][x] = 0;
+
+// definicja funkcji sprawdzającej wygraną
+function checkWin() {
+    let win_player_1 = false;
+    let win_player_2 = false;
+
+    for (let i=0; i<8; i++) {
+        for (let j=0; j<3; j++) {
+            if ( moves_player_1.indexOf(WINNING_COMBINATIONS[i][j]) > -1 && !win_player_2 ) {
+                win_player_1 = true;
+                win_player_2 = false;
+            }
+            else if ( moves_player_2.indexOf(WINNING_COMBINATIONS[i][j]) > -1 && !win_player_1 ) {
+                win_player_2 = true;
+                win_player_1 = false;
+            }
+            else {
+                win_player_1 = false;
+                win_player_2 = false;
+                break;
+            }
+        }
+        if (win_player_1 || win_player_2) break;
+        
     }
-    //Sprawdzamy czy są atrybuty pola, jesli tak to je usuwamy 
-    var zapelnione_pola = plansza.querySelectorAll("th[pole]");
-    for(var i = 0; i < zapelnione_pola.length; i++)
-        zapelnione_pola[i].removeAttribute("pole");
-    plansza.setAttribute("ruch", zaczyna);
-};
-function czy_koniec()
-{
+
     /*
-        -1 - krzyzk
-        1 - kolko,
-        -2 - remis
-        false/0 - jeszcze nie koniec
-    */
-    //Sprawdzam czy nie ma wygranej po prostej
-    for(var i = 0; i < 3; i++)
-    {
-        //Sprawdzam w poziomie
-        if(Math.abs(tablica[0][i] + tablica[1][i] + tablica[2][i]) == 3)
-            return tablica[0][i];
-        //Sprawdzam w pionie
-        if(Math.abs(tablica[i][0] + tablica[i][1] + tablica[i][2]) == 3)
-            return tablica[i][0];
+    if (turn >= 5) {
+        WINNING_COMBINATIONS.forEach((combination, index) => {
+            if (combination.every(index => moves_player_1.indexOf(index) > -1))
+                win = true;
+            else if (combination.every(index => moves_player_2.indexOf(index) > -1))
+                win = true;
+        });
     }
-    //Sprawdzam skosy
-    if(Math.abs(tablica[0][0] + tablica[1][1] + tablica[2][2]) == 3)
-        return tablica[0][0];
-    if(Math.abs(tablica[0][2] + tablica[1][1] + tablica[2][0]) == 3)
-        return tablica[0][2];
-    //Sprawdzam czy plansza nie jest zapelniona jak jest i nie ma nadal wygranej to remis
-    if(ruch_numer >= 9)
-        return -2;
-   return false;
+    */
+
+    if (win_player_1)
+        alert('Wygrał gracz 1!');
+    else if (win_player_2)
+        alert('Wygrał gracz 2!');
+    else if (turn === 9)
+        alert('Remis!');
+}
+
+// definicja funkcji klikającej
+function SimpleClick(event) {
+    if (turn % 2) {
+        moves_player_1.push(parseInt(event.target.dataset.index));
+        event.target.classList.add('times-color', 'fas', PLAYER_1);
+    }
+    else {
+        moves_player_2.push(parseInt(event.target.dataset.index));
+        event.target.classList.add('circle-color', 'far', PLAYER_2);
+    }
+
+    if (turn >= 5)
+        checkWin();
+    
+    turn++;
 }
